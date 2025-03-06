f59a3cd096741584127af1b8a1743c9a8481d445
arsalanshahs <arsalans863@deltalearns.ca>
2025-03-06 17:10:29 +0000

    pseudocode
    wrote all the pseudocode for this program so that I can follow it

---
+++ main.py
@@ -0,0 +1,21 @@
+# Enigma Machine with Atbash Cipher
+
+# Create a Function that encrypts the message
+
+
+# Create a Function that decrypts the message
+
+
+# Greet user and explain what an enigma machine is
+
+
+# Ask user if they want to encrypt, decrypt, or quit
+
+
+# If they want to encrypt, ask for a message
+
+
+# If they want to decrypt, ask for a message 
+
+
+# If they want to quit. say goodbye
