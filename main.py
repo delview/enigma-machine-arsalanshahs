990cdd4c1d1d9971ef6d6ee7840486c475421b37
arsalanshahs <arsalans863@deltalearns.ca>
2025-03-10 16:59:18 +0000

    Greet the user and explain what an enigma machine is

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 # Enigma Machine with Atbash Cipher
 
-# Create a Function that encrypts the message
+# Import the time module
+import time
 
+
+# Create a dictionary
+sipher = {"a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t", "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m", "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f", "v": "e", "w": "d", "x": "c", "y": "b", "z": "a"}
+
+# Create a Function that encrypts the message
+def encrypt(messages: list, message: str):
+    pass
 
 # Create a Function that decrypts the message
+def decrypt(messages: list, message: str):
+    pass
 
 
 # Greet user and explain what an enigma machine is
+print("Welcome to the Enigma Machine!")
+time.sleep(0.5)
+print("The Enigma Machine is a device used to encrypt and decrypt messages during World War II.")
+time.sleep(0.5)
+print("This program uses the Atbash Cipher.")
+time.sleep(0.5)
+print("The Atbash Cipher is a substitution cipher where the letters of the alphabet are reversed.")
+time.sleep(0.5)
+print("For example, the letter 'a' would be 'z', the letter 'b' would be 'y', and so on.")
+time.sleep(0.5)
+print("Let's get started!") 
+time.sleep(0.5)
 
 
 # Ask user if they want to encrypt, decrypt, or quit
